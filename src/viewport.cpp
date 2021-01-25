8d2bebdeaa2c0f15439fe69938296c405b5ce456
Michael Ershov <m.ershov@berkeley.edu>
2021-01-24 16:01:22 -0800

    tried implementing set_viewbox for task 3, didnt seem to really work. Could be due to slow speed. Move to task 4

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -10,8 +10,37 @@ void ViewportImp::set_viewbox( float x, float y, float span ) {
   // Set svg to normalized device coordinate transformation. Your input
   // arguments are defined as SVG canvans coordinates.
 
-  this->x = x;
-  this->y = y;
+  // CHECK! How did this work before we even did any coordinate transformations??
+
+  // Reflect about x matrix: identity matrix, but y is negative
+  Matrix3x3 transmat_reflection = Matrix3x3::identity(); // Get identity matrix
+  transmat_reflection[1][1] = -1; // x axis reflection
+
+  // Translation matrix: identity matrix + bx @ [0][2] + by @ [1][2]
+  Matrix3x3 transmat_translation = Matrix3x3::identity(); // Get identity matrix
+  transmat_translation[0][2] = x - span; // right shift by x - span
+  transmat_translation[1][2] = -y + span; // downshift by y - span
+
+  // Scale matrix: identity matrix + sx @ [0][0] + sy @ [1][1]
+  Matrix3x3 transmat_scale = Matrix3x3::identity(); // Get identity matrix
+  transmat_scale[0][0] = 2*span; // scale by 2*span in x
+  transmat_scale[1][1] = 2*span; // scale by 2*span in y
+
+
+  Matrix3x3 transmat = transmat_reflection * transmat_translation * transmat_scale;
+
+  Vector3D u( x, y, 1.0 );
+
+  // apply projective space transformation
+  u = transmat * u; 
+
+  // project back to 2D Euclidean plane
+  Vector2D u_out(u.x / u.z, u.y / u.z);
+
+
+
+  this->x = u_out.x;
+  this->y = u_out.y;
   this->span = span; 
 
 }
